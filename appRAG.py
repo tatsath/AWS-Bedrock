07dad308f32c03e7d1f72639298679c9e9d4937e
Hariom Tatsat <tatsatx@gmail.com>
2024-06-02 15:09:08 -0400

    Repo clean

--- appRAG.py
+++ appRAG.py
@@ -34,8 +34,8 @@ def data_ingestion():
     documents=loader.load()
 
     # - in our testing Character split works better with this PDF data set
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,
-                                                 chunk_overlap=1000)
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,
+                                                 chunk_overlap=100)
     
     docs=text_splitter.split_documents(documents)
     return docs
